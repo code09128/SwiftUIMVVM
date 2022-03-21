445a81d994d8dad8c7237b3c112b4904a0905f51
EZPDC1\dustin.hsieh <code09128@gmail.com>
2022-03-21 10:04:30 +0800

    Initial Commit

---
+++ SwiftUISearchMusicMVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUISearchMusicMVVM
+//
+//  Created by Dustin on 2022/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUISearchMusicMVVM/SwiftUISearchMusicMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISearchMusicMVVMApp.swift
+//  SwiftUISearchMusicMVVM
+//
+//  Created by Dustin on 2022/3/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISearchMusicMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
